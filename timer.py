4678611978f3602c3a0839f68c0b31d64dfd337a
someoneb100 <lawamulet@gmail.com>
2019-05-09 17:09:54 +0200

    Version 1.0

---
+++ timer.py
@@ -0,0 +1,22 @@
+from time import process_time as pt
+
+#has 4 keyword parameters
+    #pre  - function to be called before timing
+    #func - timed function
+    #post - called after timing
+    #n    - number of execution for averaging time
+
+def timer(pre = None, func = None, post = None, n = 1):
+    suma, t = 0.0, 0
+    def empty_f():
+        pass
+    a = lambda x: empty_f if x is None else x
+    pre, func, post = a(pre), a(func), a(post)
+    for _ in range(n):
+        pre()
+        temp = pt()
+        func()
+        suma += pt() - temp
+        post()
+    return suma/n
+
